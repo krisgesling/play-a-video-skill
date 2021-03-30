b258f0539818b751c4addf60d21ef2a2a1cabbac
Kris Gesling <krisgesling@gmail.com>
2021-03-30 12:21:44 +0000

    tidy up test skill

--- __init__.py
+++ __init__.py
@@ -9,29 +9,21 @@ class PlayAVideo(MycroftSkill):
 
     @intent_handler("video.a.play.intent")
     def handle_yt_video_a_play(self, message):
-        self.speak_dialog("video.a.play")
         url = self.get_video_stream()
         self.log.info(url)
         self.gui.play_video(url, "Youtube video")
 
     @intent_handler("video.test.play.intent")
     def handle_video_a_play(self, message):
-        self.speak_dialog("video.a.play")
         url = "/home/mycroft/test.mp4"
         self.log.info(url)
         self.gui.play_video(url, "Test video")
-        
 
     @staticmethod
-    def get_video_stream(url="https://www.youtube.com/watch?v=Gv1I0y6PHfg",
-                         download=False):
+    def get_video_stream(url=None):
+        if url is None:
+            url = "https://www.youtube.com/watch?v=YE7VzlLtp-4"
         stream = pafy.new(url).streams[0]
-        if download:
-            path = join(gettempdir(),
-                        url.split("watch?v=")[-1] + "." + stream.extension)
-            if not exists(path):
-                stream.download(path)
-            return path
         return stream.url
 
 def create_skill():
